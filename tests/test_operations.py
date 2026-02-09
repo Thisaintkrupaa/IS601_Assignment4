44b19e25284be6d472bc7ac18eb2b0be8cc371ed
Thisaintkrupaa <krupiikaa@gmail.com>
2026-02-09 18:18:29 -0500

    commented out previous code

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,23 +1,23 @@
-import pytest
-from app.operations import addition, subtraction, multiplication, division
+#import pytest
+#from app.operations import addition, subtraction, multiplication, division
 
-def test_addition ():
-     assert addition(1, 3) == 4
+#def test_addition ():
+     #assert addition(1, 3) == 4
    
 
-def test_subtraction():
-    assert subtraction(1, 1) == 0
+#def test_subtraction():
+   # assert subtraction(1, 1) == 0
 
 
-def test_multiplication():
-    assert multiplication(2,3) == 6
+#def test_multiplication():
+    #assert multiplication(2,3) == 6
 
-def test_division():
-    assert division(8,2) == 4
+#def test_division():
+    #assert division(8,2) == 4
 
-    def test_division_positive():
-        assert division(6, 2) == 3
+    #def test_division_positive():
+       # assert division(6, 2) == 3
 
-    def test_division_negative():
-        with pytest.raises(ValueError, match="Division by zero is not allowed"):
-            division(1, 0)  
+    #def test_division_negative():
+       # with pytest.raises(ValueError, match="Division by zero is not allowed"):
+            #division(1, 0)  
