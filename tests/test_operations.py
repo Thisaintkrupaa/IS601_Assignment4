1de3b6f808190cd1656b5d86836b370ad839e8c5
Thisaintkrupaa <krupiikaa@gmail.com>
2026-02-09 18:22:21 -0500

    added pytest addition

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,3 +1,90 @@
+import pytest
+from typing import Union
+from app.operations import Operations
+
+Number = Union[int, float]
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (2, 3, 5),           
+        (0, 0, 0),           
+        (-1, 1, 0),          
+        (2.5, 3.5, 6.0),     
+        (-2.5, 3.5, 1.0),    
+    ],
+
+ids=[
+        "add_two_positive_integers",
+        "add_two_zeros",
+        "add_negative_and_positive_integer",
+        "add_two_positive_floats",
+        "add_negative_float_and_positive_float",
+    ]
+)
+
+def test_addition(a: Number, b: Number, expected: Number) -> None:
+     result = Operations.addition(a, b)
+     assert result == expected, f"Expected addition({a}, {b}) to be {expected}, but got {result}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #import pytest
 #from app.operations import addition, subtraction, multiplication, division
 
