2b9be942a5d0103cd32aeb9509a0634ca19c705d
Thisaintkrupaa <krupiikaa@gmail.com>
2026-02-17 20:54:02 -0500

    update operation code

--- app/operations/__init__.py
+++ app/operations/__init__.py
@@ -1,6 +1,4 @@
-
-class Operations:
-    """Provides basic arithmetic operations using static methods."""
+class Operation:
 
     @staticmethod
     def addition(a: float, b: float) -> float:
@@ -20,19 +18,6 @@ class Operations:
             raise ValueError("Division by zero is not allowed.")
         return a / b
 
-
-
-
-#def addition(a: float, b: float) -> float:
-    #return a + b
-
-#def subtraction(a: float, b: float) -> float:
-    #return a - b
-
-#def multiplication(a: float, b: float) -> float:
-   # return a * b
-
-#def division(a: float, b: float) -> float:
-    #if b == 0:
- # raise ValueError("Cannot divide by zero")
-    #return a / b 
+    @staticmethod
+    def power(a: float, b: float) -> float:
+        return a ** b
